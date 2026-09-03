d3ec8231259118ce18ca5253ef8d4818df47acab
Manjeet <manjeetsingh.wrk@gmail.com>
2026-09-03 22:25:50 +0530

    Enhance errorHandler to handle unknown errors
    Refactor error handling to check for ErrorResponse instance and provide a generic response for unhandled errors.

--- src/core/middleware/error.ts
+++ src/core/middleware/error.ts
@@ -1,16 +1,28 @@
 // internal-imports
-import type { ErrorResponse } from '../response/error.js';
+import { ErrorResponse } from '../response/error.js';
 
 // external-imports
 import type { NextFunction, Request, Response } from 'express';
 
 // middleware function to handle errors
 export function errorHandler(
-  error: ErrorResponse,
+  error: unknown,
   _request: Request,
   response: Response,
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   _next: NextFunction
 ) {
-  return response.status(error.code).json(error);
+  // check if error is an instance of ErrorResponse
+  if (error instanceof ErrorResponse)
+    return response.status(error.code).json({
+      success: false,
+      message: error.message,
+      issues: error.issues,
+    });
+
+  // return a generic error response for unhandled errors
+  return response.status(500).json({
+    success: false,
+    message: 'Internal Server Error',
+  });
 }
